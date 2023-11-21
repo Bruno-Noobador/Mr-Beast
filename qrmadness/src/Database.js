eccba1d9aaa23be7a226a0c635bc9a60cc80502e
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-11-21 13:40:28 +0000

    Added (fixed)database, raised racism levels

---
+++ qrmadness/src/Database.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+(async () => {
+    const db = await open({
+        filename: './db/db.db',
+        driver: sqlite3.cached.Database
+    })
+
+
+    // const results = await db.exec(`create table users(
+    //     id integer primary key,
+    //     name text
+    // )`)
+
+    // const results = await db.exec(`create table admin(
+    //     id integer primary key,
+    //     name text
+    // )`)
+
+    const results = await db.exec(`create table locations(
+        id integer primary key,
+        name text,
+        key text,
+        user_id integer,
+        arrivalTime datetime,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )`)
+
+    console.log(results)
+})()
