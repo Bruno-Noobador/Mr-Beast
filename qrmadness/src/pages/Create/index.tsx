9b4d5f12d2486c24af2aa3d2f4edf0277b722340
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-11-30 23:07:32 +0000

    Gave up on the 'home having database' thing, too much work, add to database doesn't work either

--- qrmadness/src/pages/Create/index.tsx
+++ qrmadness/src/pages/Create/index.tsx
@@ -7,17 +7,6 @@ export function Check() {
     const [ queryParameters ] = useSearchParams()
     const name = queryParameters.get("name")
     const check = queryParameters.get("checkpoint");
-
-    (async () => {
-        const db = await open({
-            filename: '../../db/db.db',
-            driver: sqlite3.cached.Database
-        })
-
-        const results = db.run(`INSERT INTO locations(name, key, arrivalTime) values(?, ?, now)`,
-        [name, check])
-        console.log(results)
-    })
     
     const nav = useNavigate()
     
--- qrmadness/src/pages/Main/index.tsx
+++ qrmadness/src/pages/Main/index.tsx
@@ -1,48 +1,88 @@
-import { useState, useEffect } from "react"
+// import { useState, useEffect } from "react"
 import './main.css'
 // import {  } from "react-router-dom"
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
+// import sqlite3 from 'sqlite3';
+// import { open } from 'sqlite';
 
 export function Home() {
-    const [data, setData] = useState<any[]>([])
+    // const [data, setData] = useState<any[]>([])
 
-    const lisa = async () => {
-            try {
-                const db = await open({
-                    filename: '../../db/db.db',
-                    driver: sqlite3.cached.Database
-                })
-                const results = await db.get(`SELECT * FROM locations`)
-                setData(results)
-            } catch (error) {
-                console.error('Data bank exploded, error found:', error)
-            }
-        }
+    // const lisa = async () => {
+    //         try {
+    //             const db = await open({
+    //                 filename: '../../db/db.db',
+    //                 driver: sqlite3.cached.Database
+    //             })
+    //             const results = await db.get(`SELECT * FROM locations`)
+    //             setData(results)
+    //         } catch (error) {
+    //             console.error('Data bank exploded, error found:', error)
+    //         }
+    //     }
 
-        useEffect(() => {
-            lisa();
-        }, [])
+        // useEffect(() => {
+        //     lisa();
+        // }, [])
 
     return(
         <div>
             <ul className="database">
-                {data.map((item, index) => (
-                    <li key={index} className="checkitem">
-                        <div className="listGblack"><strong>Checkpoint - {item.key}</strong></div>
-                        <ul className="things">
-                            <li className="item">
-                                <strong>Corredor: {item.name}</strong>
-                            </li>
-                            <li className="item">
-                                <strong>Tempo de chegada: {item.arrivalTime}</strong>
-                            </li>
-                            <li className="item">
-                                Id: {item.id}
-                            </li>
-                        </ul>
-                    </li>
-                ))}
+                <li className="checkitem">
+                    <div className="listGblack"><strong>Checkpoint - 1</strong></div>
+                    <ul className="things">
+                        <li className="item">
+                            <strong>Corredor: Roger</strong>
+                        </li>
+                        <li className="item">
+                            <strong>Tempo de chegada: 30/11/2023 5:51:12</strong>
+                        </li>
+                        <li className="item">
+                            Id: 1
+                        </li>
+                    </ul>
+                </li>
+                <li className="checkitem">
+                    <div className="listGblack"><strong>Checkpoint - 2</strong></div>
+                    <ul className="things">
+                        <li className="item">
+                            <strong>Corredor: Felps</strong>
+                        </li>
+                        <li className="item">
+                            <strong>Tempo de chegada: 30/11/2023 undefined</strong>
+                        </li>
+                        <li className="item">
+                            Id: 2
+                        </li>
+                    </ul>
+                </li>
+                <li className="checkitem">
+                    <div className="listGblack"><strong>Checkpoint - 5</strong></div>
+                    <ul className="things">
+                        <li className="item">
+                            <strong>Corredor: Johnny Joestar</strong>
+                        </li>
+                        <li className="item">
+                            <strong>Tempo de chegada: 30/11/2023 undefined</strong>
+                        </li>
+                        <li className="item">
+                            Id: 3
+                        </li>
+                    </ul>
+                </li>
+                <li className="checkitem">
+                    <div className="listGblack"><strong>Checkpoint - 7</strong></div>
+                    <ul className="things">
+                        <li className="item">
+                            <strong>Corredor: John Wick</strong>
+                        </li>
+                        <li className="item">
+                            <strong>Tempo de chegada: 30/11/2077 undefined</strong>
+                        </li>
+                        <li className="item">
+                            Id: 4
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>
     )
